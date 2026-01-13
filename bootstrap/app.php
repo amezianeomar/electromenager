78ea0b9047de19dd495ef957213252cfcfb1c2d1
ameziane <imorafid@gmail.com>
2026-01-13 17:53:43 +0100

    configuration vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->trustProxies(
+            '*',
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO
+);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
